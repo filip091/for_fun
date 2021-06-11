642ab327cee3f9323671cc3c9787cfdcce7f5ded
filip0913 <fedrexler@gmail.com>
2021-06-11 10:11:00 +0200

    new file:   hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,24 @@
+// prints hello world using C
+
+#include <stdio.h>
+
+int main()
+{
+	printf("C   C  CCCCC  C      C      CCCCC\n");
+	printf("C   C  C      C      C      C   C\n");
+	printf("C   C  C      C      C      C   C\n");
+	printf("CCCCC  CCC    C      C      C   C\n");
+	printf("C   C  C      C      C      C   C\n");
+	printf("C   C  C      C      C      C   C\n");
+	printf("C   C  CCCCC  CCCCC  CCCCC  CCCCC\n");
+	printf("\n");
+	printf("C   C  CCCCC  CCCCC  C      CCCC \n");
+	printf("C   C  C   C  C   C  C      C   C\n");
+	printf("C C C  C   C  CCCCC  C      C   C\n");
+	printf("CC CC  C   C  CC     C      C   C\n");
+	printf("C   C  C   C  C C    C      C   C\n");
+	printf("C   C  C   C  C  C   C      C   C\n");
+	printf("C   C  CCCCC  C   C  CCCCC  CCCC \n");
+
+	return 0;
+}
